f579e1836a2c9edb84893a8741bebab40c2a55c0
Khalil <khalilpati55@gmail.com>
2024-09-26 17:09:00 +0530

    Used the custom hook and sonner to run create board mutation

--- src/app/(dashboard)/_components/empty-states/board.tsx
+++ src/app/(dashboard)/_components/empty-states/board.tsx
@@ -1,9 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import Board from "@/assets/plan.svg";
 import { Button } from "@/components/ui/button";
+import { api } from "@convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import useConvexMutation from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
 
 const EmptyBoard = () => {
+  const { pending, mutate: create } = useConvexMutation(api.boards.create);
+  const { organization } = useOrganization();
+
+  const handleClick = async () => {
+    if (!organization) return;
+
+    toast.promise(
+      async () =>
+        await create({
+          title: "My First Board",
+          orgId: organization.id,
+        }),
+      {
+        loading: "Creating board...",
+        success: (data) => {
+          console.log(data);
+          return "Board created!";
+        },
+        error: "Failed to create board",
+      }
+    );
+  };
+
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
       <Image src={Board} alt="No Favorite Boards" height={140} width={140} />
@@ -12,7 +41,9 @@ const EmptyBoard = () => {
         Start by creating a new board
       </p>
       <div className="mt-6">
-        <Button size="lg">Create Board</Button>
+        <Button size="lg" disabled={pending} onClick={handleClick}>
+          Create Board
+        </Button>
       </div>
     </div>
   );
