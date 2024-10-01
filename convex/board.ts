2786e2137d1d0bfc5f8f25c5d2aef55d2eb9dc46
Khalil <khalilpati55@gmail.com>
2024-10-02 01:59:56 +0530

    Added query to fetch board

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 const images = [
   "/placeholders/1.svg",
@@ -147,5 +147,14 @@ export const unfavorite = mutation({
     await ctx.db.delete(existingFavorite._id);
 
     return board;
+  },
+});
+
+export const get = query({
+  args: {
+    id: v.id("boards"),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.get(args.id);
   },
 });
